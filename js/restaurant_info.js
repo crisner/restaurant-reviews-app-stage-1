6929340fd111f7283bf2dc665610be63ef7f3a3a
Renisha Christie. A <christie.reni@gmail.com>
2018-10-02 18:03:34 +0530

    Add aria labels for breadcrumbs

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -180,6 +180,7 @@ fillBreadcrumb = (restaurant=self.restaurant) => {
   const li = document.createElement('li');
   li.innerHTML = restaurant.name;
   li.setAttribute('tabindex', 1);
+  li.setAttribute('aria-current', 'page');
   breadcrumb.appendChild(li);
 }
 
