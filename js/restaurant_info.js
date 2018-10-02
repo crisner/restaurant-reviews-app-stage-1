9a7d3a89dd8680c759343e15e73163f2bfee0951
Renisha Christie. A <christie.reni@gmail.com>
2018-10-02 17:41:51 +0530

    Change review heading to h3

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -128,7 +128,7 @@ fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => 
  */
 fillReviewsHTML = (reviews = self.restaurant.reviews) => {
   const container = document.getElementById('reviews-container');
-  const title = document.createElement('h2');
+  const title = document.createElement('h3');
   title.innerHTML = 'Reviews';
   title.setAttribute('tabindex', 1);
   title.setAttribute('role', 'heading');
